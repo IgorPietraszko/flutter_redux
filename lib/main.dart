654fe3bdd17e0e1cfe7ff7c4d30cc6bd1e5f3a85
Igor Pietraszko <pietraszko.igor@outlook.com>
2018-03-29 16:07:35 -0600

    stopped at 18:24

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+import 'package:redux/redux.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+
+// following this youtube video: https://youtu.be/X8B-UzqEaWc
 
 void main() => runApp(new MyApp());
+
+@immutable
+class AppState {
+  final counter;
+  AppState(this.counter);
+}
+
+// actions
+enum Actions { increment }
+
+// pure function
+AppState reducer(AppState previousState, action) {
+  if(action == Actions.increment) {
+    return new AppState(previousState.counter+1);
+  }
+  return previousState;
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -9,14 +31,6 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: new MyHomePage(),
@@ -28,35 +42,12 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: new Text("Flutter Redux"),
       ),
       body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             new Text(
@@ -73,7 +64,7 @@ class MyHomePage extends StatelessWidget {
         onPressed: (){},
         tooltip: 'Increment',
         child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), 
     );
   }
 }
