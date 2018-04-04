07800662b9951e8e9c5390f13b3af2973e4277f8
Igor Pietraszko <pietraszko.igor@outlook.com>
2018-04-04 16:03:35 -0600

    Added proxy authentication stuff.  Fighting with different versions of cloud firestone to get it to compile

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,9 @@ import 'package:redux/redux.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-// following this youtube video: https://youtu.be/X8B-UzqEaWc
+// following this youtube video for basic app: https://youtu.be/X8B-UzqEaWc
+// following this article for firebase: https://medium.com/shift-studio/flutter-redux-and-firebase-cloud-firestore-in-sync-2c1accabdac4
+
 
 void main() => runApp(new MyApp());
 
@@ -29,22 +31,6 @@ AppState reducer(AppState previousState, action) {
   return previousState;
 }
 
-  /// A function that will be run when the StoreConnector is initially created.
-  /// It is run in the [State.initState] method.
-  ///
-  /// This can be useful for dispatching actions that fetch data for your Widget
-  /// when it is first displayed.
-  final OnInitCallback onInit;
-
-  /// A function that will be run when the StoreBuilder is removed from the
-  /// Widget Tree.
-  ///
-  /// It is run in the [State.dispose] method.
-  ///
-  /// This can be useful for dispatching actions that remove stale data from
-  /// your State tree.
-  final OnDisposeCallback onDispose;
-
 class MyApp extends StatelessWidget {
   
   final store = new Store(reducer, initialState: new AppState(0));
@@ -67,36 +53,40 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Flutter Redux"),
-        ),
-        body: new Center(
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Text(
-                'You have pushed the button this many times:',
-              ),
-              new StoreConnector(
-                converter: (store) => store.state.counter,
-                builder: (context, counter) => new Text(
-                  '$counter',
-                  style: Theme.of(context).textTheme.display1,
-                ),
-              ),
-            ],
+    return new StoreBuilder(
+      builder: (context, Store<AppState> store) {
+        return new Scaffold(
+          appBar: new AppBar(
+            title: new Text("Flutter Redux"),
           ),
-        ),
-        floatingActionButton: new StoreConnector<int, VoidCallback>(
-          converter: (store) {
-            return () => store.dispatch(Actions.increment);},
-          builder: (context, callback) =>  new FloatingActionButton(
-            onPressed: callback,
-            tooltip: 'Increment',
-            child: new Icon(Icons.add),
-        ),
-        ), 
+          body: new Center(
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Text(
+                  'You have pushed the button this many times:',
+                ),
+                new StoreConnector(
+                  converter: (store) => store.state.counter,
+                  builder: (context, counter) => new Text(
+                    '$counter',
+                    style: Theme.of(context).textTheme.display1,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          floatingActionButton: new StoreConnector<int, VoidCallback>(
+            converter: (store) {
+              return () => store.dispatch(Actions.increment);},
+            builder: (context, callback) =>  new FloatingActionButton(
+              onPressed: callback,
+              tooltip: 'Increment',
+              child: new Icon(Icons.add),
+          ),
+          ), 
+        );
+      },
     );
   }
 }
